1c720218a1e60d38a04b9605087f3d4d101aa3db
unknown <slash0303>
2026-03-06 04:00:26 +0900

    fix: add null/undef check in createElement function

--- components/createElement.js
+++ components/createElement.js
@@ -2,11 +2,12 @@
 const createElement = (tagName, props, innerHTML = null) => {
     const element = document.createElement(tagName);
     
-    const propsKeys = Object.keys(props);
-
-    propsKeys.forEach((key) => {
-        element.setAttribute(key, props[key]);
-    });
+    if(props != undefined || props != null){
+        const propsKeys = Object.keys(props);
+        propsKeys.forEach((key) => {
+            element.setAttribute(key, props[key]);
+        });
+    }
 
     element.innerHTML = innerHTML;
 
